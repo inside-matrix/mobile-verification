f1275e4335836fb4569c152802cbf34f9719b79b
Harshit Arya <mjbj.anand@gmail.com>
2018-12-02 23:13:31 +0530

    OTP Verifcation

--- lib/index.ts
+++ lib/index.ts
@@ -12,7 +12,6 @@ export class MobileVerification implements WebAppInterface {
 	public constructor() {
 		let mobileTextField = document.querySelector('#send_otp_section .mdc-text-field.mobile');
 		this.mobileField = new MDCTextField(mobileTextField);
-		// this.countryField = new MDCTextField(document.querySelector('.mdc-text-field.country'));
 
 		this.sendOtpButton = document.querySelector('#send_otp_section .mdc-button.button');
 		let buttonRipple = new MDCRipple(this.sendOtpButton);
@@ -22,15 +21,7 @@ export class MobileVerification implements WebAppInterface {
 		this.otpField = new MDCTextField(otpTextField);
 		let verifyOtpButton = document.querySelector('#verify_otp_section .mdc-button.button');
 		let verifyButtonField = new MDCRipple(verifyOtpButton);
-		verifyOtpButton.addEventListener(
-			'click',
-			function() {
-				let otp = document.getElementById('otp') as HTMLInputElement;
-				let otpValue = otp.value;
-
-				(this as MobileVerification).verifyOTP(otpValue);
-			}.bind(this)
-		);
+		verifyOtpButton.addEventListener('click', this.onVerifyOTP.bind(this));
 	}
 
 	private onSendOTP() {
@@ -38,10 +29,17 @@ export class MobileVerification implements WebAppInterface {
 		this.sendOtpButton.className += ' disable';
 		this.sendOtpButton.innerText = 'Sending OTP';
 
-		let mobileInput = document.getElementById('mobile_no') as HTMLInputElement;
+		let mobileInput = document.getElementById('sasapp_device_no') as HTMLInputElement;
 		this.mobileNo = mobileInput.value;
 
 		this.sendOTP(this.mobileNo);
+	}
+
+	private onVerifyOTP() {
+		let otp = document.getElementById('sasapp_otp') as HTMLInputElement;
+		let otpValue = otp.value;
+
+		this.verifyOTP(otpValue);
 	}
 
 	private sendOTP(number: string): void {
@@ -58,10 +56,6 @@ export class MobileVerification implements WebAppInterface {
 
 	private showStatusMsg(msg: string, type: 'Error' | 'Information' | 'Success') {
 		window.AnandApp.showSnack(type + ': ' + msg);
-		// let msgElement = document.getElementById('status-msg');
-
-		// msgElement.innerText = msg;
-		// msgElement.className = 'mdc-typography status-msg ' + type;
 	}
 
 	public OTPSent(response: string): void {
@@ -69,13 +63,12 @@ export class MobileVerification implements WebAppInterface {
 		document.getElementById('send_otp_section').style.display = 'none';
 		document.getElementById('verify_otp_section').style.display = 'block';
 		document.getElementById('otp_sent_on').innerText += this.mobileNo;
-		//Show OTP box and hide other
 	}
 
 	public OTPVerified(response: string, number: string): void {
 		console.log(response);
 		localStorage.setItem(Values.VERIFIED_MOBILE_NO_KEY, number);
-		window.location.href = '/';
+		window.location.replace('/');
 	}
 
 	public OTPSendFailed(error: string): void {
@@ -100,24 +93,6 @@ export class MobileVerification implements WebAppInterface {
 	}
 
 	public appDidBecomeActive(): void {}
-
-	// private sendOTP(mobileNo: string): void {
-	// 	let xhttp = new XMLHttpRequest();
-	// 	xhttp.onreadystatechange = function() {
-	// 		if (this.readyState == 4) {
-	// 			if (this.status == 200) {
-	// 				let response = JSON.parse(this.responseText);
-	// 				console.log(response);
-	// 			}
-	// 		}
-	// 	};
-
-	// 	xhttp.open('POST', '/mobile_send_otp', true);
-	// 	let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content');
-	// 	xhttp.setRequestHeader('X-CSRF-TOKEN', csrfToken);
-	// 	xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	// 	xhttp.send('mobile_no=' + mobileNo);
-	// }
 }
 
 let instance: MobileVerification = new MobileVerification();
